8da37fe262ea5b8809390d9ffd428b26e3f39bd9
Albert Jr <69417978+r3wind29@users.noreply.github.com>
2020-10-01 18:14:01 +0800

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '643bf25b85b488903b168ef27d1d3a53';
 
 // Set Position function
 setPosition();
